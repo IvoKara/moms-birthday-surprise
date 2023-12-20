160fddf04e7fb553febf75fedcbdcc7945980ac7
Ivo <ivo.i.karaneshev@gmail.com>
2023-12-21 01:17:37 +0200

    feat: update footer && apply changes to default layout

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer opacity-85 text="sm">
+    <p>
+      Направено с много
+      <span aria-label="любов">
+        ❤️
+      </span>
+      от Ивко
+    </p>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,8 @@
 <template>
-  <main class="px-10 py-20 text-center">
-    <slot />
-    <Footer />
-    <div class="mx-auto mt-5 text-center text-sm opacity-25">
-      [Default Layout]
+  <main h-full px-10 py-20 text-center>
+    <div h-full grid="~ cols-1" place-items-center>
+      <slot />
     </div>
+    <Footer />
   </main>
 </template>
