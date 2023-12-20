fecbae83c66fd62ef34a6c12ae7132210678c21d
Ivo <ivo.i.karaneshev@gmail.com>
2023-12-20 22:06:35 +0200

    chore: change constats - name && descr

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appName = 'Честит рожден ден, мамо!'
+export const appDescription = 'Направено с огромна любов от мен за теб'
