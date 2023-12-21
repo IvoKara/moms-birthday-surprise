85fb3b0d51a0564eeb5e3c12536c45cbfab2d9aa
Ivo <ivo.i.karaneshev@gmail.com>
2023-12-21 04:08:26 +0200

    feat: present animation with blur

---
+++ components/Present.vue
@@ -0,0 +1,29 @@
+<template>
+  <div isolate relative aspect-square max-h-90 p-5>
+    <div absolute inset-0 z--1 rounded="50%" bg-amber blur-3xl />
+
+    <NuxtImg class="animate-up-down" preload src="/present.png" alt="Подарък" />
+  </div>
+</template>
+
+<style>
+@keyframes up-down {
+  from,
+  to {
+    transform: translate3d(0, 0, 0);
+  }
+
+  25% {
+    transform: translate3d(0, 10px, 0);
+  }
+
+  75% {
+    transform: translate3d(0, -10px, 0);
+  }
+}
+
+.animate-up-down {
+  animation: up-down 6s ease-in-out infinite;
+  animation-fill-mode: both;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div flex="~ col" items-center gap-20>
+    <Present />
+
     <Button>Отвори ме!</Button>
   </div>
 </template>
